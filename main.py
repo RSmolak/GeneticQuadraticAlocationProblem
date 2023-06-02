4cc54749175ad95aeeb6eb0950b58066d7bc65f6
RSmolak <rafal.smolak@gmail.com>
2023-06-02 10:49:00 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+
+
+def calc_target(solution, flow, distance):
+    return sum(flow[i][j] * distance[k][l] * solution[i][k] * solution[j][l] for i in range(n) for j in range(n) for k in range(n) for l in range(n))
+
+# hyperparameters
+n = 4
+flow = [[random.randint(1,50) for _ in range(n)] for _ in range(n)] # przepływ pomiędzy zakładami
+distance = [[random.randint(1,50) for _ in range(n)] for _ in range(n)]  # odległość pomiędzy zakładami
+solution = [[0 for j in range(n)] for i in range(n)]
+
